9158e2f89023ff9b34fc794925376180c859b74f
Никита Матеюк <work.acr0matic@gmail.com>
2023-01-16 14:32:52 +0300

    Initial commit

---
+++ gulp_tasks/layout.js
@@ -0,0 +1,21 @@
+const { task, src, dest } = require('gulp');
+
+const useref = require('gulp-useref');
+const htmlMin = require('gulp-htmlmin');
+
+const paths = require('../gulpfile');
+
+/*
+- Переименовывание ассетов
+- Минификация и сортировка разметки
+*/
+
+task('layout', () => src(paths.html.src)
+  .pipe(useref({ searchPath: [paths.styles.src, paths.scripts.src] }))
+  .pipe(htmlMin({
+    sortAttributes: true,
+    sortClassName: true,
+    removeComments: true, // Отключить, если требуется посадка верстки на CMS
+    collapseWhitespace: true, // Отключить, если требуется посадка верстки на CMS
+  }))
+  .pipe(dest(paths.html.dist)));
