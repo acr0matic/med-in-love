57592640d61ed32f8eee3bd295459e0329dd68c9
acr0matic <work.acr0matic@gmail.com>
2023-01-19 17:29:28 +0300

    feat: портфолио, отзывы

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -8,3 +8,27 @@ const lazyLoadInstance = new LazyLoad({
     if (loader) loader.classList.add('preloader--hide');
   },
 });
+
+const portfolioHome = new Swiper('.slider-portfolio-home', {
+  slidesPerView: 1.6,
+  centeredSlides: true,
+  spaceBetween: 30,
+
+  grabCursor: true,
+
+  // mousewheel: {
+  //   releaseOnEdges: true,
+  // },
+});
+
+const feedbackHome = new Swiper('.slider-feedback-home', {
+  slidesPerView: 1.6,
+  centeredSlides: true,
+  spaceBetween: 30,
+
+  grabCursor: true,
+
+  // mousewheel: {
+  //   releaseOnEdges: true,
+  // },
+});
